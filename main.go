581c8b1f0491bc1ce6b6ec80d4ffd26142f70e94
Brian Sonnenberg <bsonnenberg@google.com>
2025-10-28 18:24:24 +0000

    Update after switching to bytes for Pod proto

--- main.go
+++ main.go
@@ -2,31 +2,32 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"io"
 	"log"
 	"net"
-	"runtime"
 	"strings"
 	"sync"
+	"time"
 
-	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/protobuf/encoding/prototext"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/proto"
+	v1 "k8s.io/api/core/v1"
 	podsv1alpha1 "k8s.io/kubelet/pkg/apis/pods/v1alpha1"
 )
 
 const (
-	socketPath = "/var/lib/kubelet/pods-api/pods-api.sock"
-	// Updated to use the key you have implemented on the server side.
+	socketPath           = "/var/lib/kubelet/pods-api/pods-api.sock"
 	fieldMaskMetadataKey = "x-kubernetes-fieldmask"
 )
 
 // watchResult holds the outcome from a single event from one of the watch streams.
 type watchResult struct {
 	watcherName string
-	event       *podsv1alpha1.WatchPodsEvent
+	eventType   podsv1alpha1.EventType
+	pod         *v1.Pod
 	messageSize int
 }
 
@@ -46,7 +47,6 @@ func watch(
 
 	reqCtx := ctx
 	if len(maskPaths) > 0 {
-		// The value is a comma-separated list of field paths.
 		headerValue := strings.Join(maskPaths, ",")
 		log.Printf("[%s] Attaching metadata: '%s: %s'", watcherName, fieldMaskMetadataKey, headerValue)
 
@@ -84,9 +84,16 @@ func watch(
 			continue
 		}
 		size := proto.Size(event)
+		pod := &v1.Pod{}
+		if err := pod.Unmarshal(event.GetPod()); err != nil {
+			log.Printf("[%s] Failed to decode pod: %v", watcherName, err)
+			continue
+		}
+
 		results <- watchResult{
 			watcherName: watcherName,
-			event:       event,
+			eventType:   event.GetType(),
+			pod:         pod,
 			messageSize: size,
 		}
 	}
@@ -134,6 +141,55 @@ func main() {
 	var wg sync.WaitGroup
 	results := make(chan watchResult)
 
+	// Periodic List and Get checks
+	go func() {
+		ticker := time.NewTicker(5 * time.Second)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				log.Println("--- Periodic List Check ---")
+				listResp, err := client.ListPods(ctx, &podsv1alpha1.ListPodsRequest{})
+				if err != nil {
+					log.Printf("ListPods failed: %v", err)
+					continue
+				}
+
+				podsList := listResp.GetPods()
+				log.Printf("ListPods returned %d pods", len(podsList))
+
+				if len(podsList) > 0 {
+					p := &v1.Pod{}
+					// Assuming the list returns items compatible with Unmarshal (likely []byte)
+					if err := p.Unmarshal(podsList[0]); err != nil {
+						log.Printf("Failed to unmarshal first pod from list: %v", err)
+						continue
+					}
+
+					log.Printf("Attempting GetPod for %s/%s (UID: %s)", p.Namespace, p.Name, p.UID)
+					getResp, err := client.GetPod(ctx, &podsv1alpha1.GetPodRequest{
+						PodUID: string(p.UID),
+					})
+					if err != nil {
+						log.Printf("GetPod failed: %v", err)
+						continue
+					}
+
+					p2 := &v1.Pod{}
+					if err := p2.Unmarshal(getResp.GetPod()); err != nil {
+						log.Printf("Failed to unmarshal pod from GetPod: %v", err)
+						continue
+					}
+					b, _ := json.MarshalIndent(p2, "", "  ")
+					log.Printf("Successfully performed GetPod for %s/%s:\n%s", p2.Namespace, p2.Name, string(b))
+				}
+			}
+		}
+	}()
+
 	wg.Add(2)
 	// Launch the unmasked watch (sends no metadata)
 	go watch(ctx, &wg, "Unmasked  ", client, nil, results)
@@ -149,12 +205,17 @@ func main() {
 	log.Println("Both watch streams started. Waiting for pod events...")
 
 	for result := range results {
-		prettyEvent := prototext.Format(result.event)
+		b, err := json.MarshalIndent(result.pod, "", "  ")
+		if err != nil {
+			log.Printf("Failed to marshal pod: %v", err)
+			continue
+		}
+		prettyPod := string(b)
 		log.Printf("EVENT %s [%s]: Size: %4d bytes\n---\n%s\n---",
-			result.event.GetType(),
+			result.eventType,
 			result.watcherName,
 			result.messageSize,
-			prettyEvent,
+			prettyPod,
 		)
 	}
 
